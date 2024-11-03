3c160372aa7b7e939efb47808564713466edf1f7
pedrojaraujo <pedroa990@gmail.com>
2024-11-03 16:52:54 -0300

    refactor: remover componente Icon e atualizar página de perfil com novos estilos e ícones

--- src/pages/Perfil/style.js
+++ src/pages/Perfil/style.js
@@ -1,5 +1,44 @@
 import styled from 'styled-components';
 
-export const PerfilContainer = styled.section`
-  height: 720px;
+export const ProfileSection = styled.section`
+  margin-top: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #000;
+  width: 80%;
+  padding: 3cqmin 20px;
+  border-radius: 16px;
+`;
+
+export const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+export const BoxSection = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  flex-wrap: wrap;
+
+  h1 {
+    font-weight: 700;
+  }
+`;
+
+export const ProfileBox = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 400px;
+  height: 128px;
+  background: black;
+  border-radius: 16px;
+  color: white;
+  align-items: center;
+  padding: 20px;
 `;
